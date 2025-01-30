a0da83e637b5aa45c8a13c526c59381548e3f68a
anaresgalla <anaresgalla@gmail.com>
2025-01-30 11:24:24 -0300

    Creates Users Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_30_134257) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_30_141644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "password_digest", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
